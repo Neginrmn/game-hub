8854be0473d267fffd384c917aee0a2712dbc47b
neginrmn <negin.pirmohammadi@gmail.com>
2023-08-30 14:45:15 -0400

    implement dark mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,28 @@
-import { Button, ButtonGroup } from "@chakra-ui/react";
+import { Grid, GridItem, Show } from "@chakra-ui/react";
+import { NavBar } from "./components/NavBar";
 function App() {
-  return <Button colorScheme="blue">Button</Button>;
+  return (
+    <Grid
+      templateAreas={{
+        base: `"nav" "main"`,
+        lg: `"nav nav" "aside main"`, //1024
+      }}
+    >
+      <GridItem area="nav">
+        <NavBar />
+      </GridItem>
+      <Show above="lg">
+        {" "}
+        <GridItem area="aside" bg="#ba92">
+          Aside
+        </GridItem>
+      </Show>
+
+      <GridItem area="main" bg="#6765">
+        Main
+      </GridItem>
+    </Grid>
+  );
 }
 
 export default App;
